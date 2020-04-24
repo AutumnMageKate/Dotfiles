f02ba1e66fcba4b23f4d6e2f3a4df591b07a26db
Kate Bertelsen <kate@rememberautumn.com>
2020-04-24 17:12:45 -0500

    Document adding Minecraft to touchbar exception list

---
+++ macos_defaults.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# There are some settings that just can't be done from within the various settings dialogs
+# This file exists to document them
+
+# Have Minecraft (and all Java apps) default to the list of apps that default the touchbar to function keys
+
+# This needs to only be run once
+# defaults write com.apple.touchbar.agent PresentationModePerApp -dict-add net.java.openjdk.cmd functionKeys
